ffe09603fe61656a1300eaae0f0c7b3f4e6d7c83
Lorenzo Calisti <djonline96@gmail.com>
2021-06-22 17:55:51 +0200

    Add more structure to the dapp

--- dapp/index.js
+++ dapp/index.js
@@ -1,18 +1,40 @@
 window.onload = () => {
-    document.getElementById('execute-btn').onclick = () => {
-        const web3 = new Web3('http://localhost:8545');
-        //const addr = '0xc9F26f69e0771E697aAbE4E8Be5F097aDf76D783';
-        const contractAddr = document.getElementById('contract-addr-input').value;
-        const userAddr = document.getElementById('user-contract-addr-input').value;
+    // Set the contract info
+    document.getElementById('token-name-txt').innerHTML = "StepToken - STP";
+    document.getElementById('token-value-txt').innerHTML = "0.6 $/STP";
 
-        const contract = getContract(web3, contractAddr);
-        contract.methods.getInteger().call({ from: userAddr })
-            .then((result) => {
-                document.getElementById('label').innerHTML = result;
-            }).catch((err) => {
-                console.error(err);
-            });
-    }
+    // document.getElementById('execute-btn').onclick = () => {
+    //     const web3 = new Web3('http://localhost:8545');
+    //     //const addr = '0xc9F26f69e0771E697aAbE4E8Be5F097aDf76D783';
+    //     const contractAddr = document.getElementById('contract-addr-input').value;
+    //     const userAddr = document.getElementById('user-addr-input').value;
+
+    //     const contract = getContract(web3, contractAddr);
+    //     contract.methods.getInteger().call({ from: userAddr })
+    //         .then((result) => {
+    //             console.log(result);
+    //             //document.getElementById('label').innerHTML = result;
+    //         }).catch((err) => {
+    //             console.error(err);
+    //         });
+    // }
+};
+
+document.getElementById('current-balance-btn').onclick = () => {
+    document.getElementById('current-balance-txt').innerHTML = "123456 STP";
+};
+
+document.getElementById('transfer-btn').onclick = () => {
+    transferTo = document.getElementById('transfer-to-input').value;
+    transferAmount = document.getElementById('transfer-amount-input').value;
+    console.log(transferAmount+" "+transferTo);
+};
+
+document.getElementById('transfer-from-btn').onclick = () => {
+    transferFrom = document.getElementById('transfer-from-from-input').value;
+    transferTo = document.getElementById('transfer-from-to-input').value;
+    transferAmount = document.getElementById('transfer-from-amount-input').value;
+    console.log(transferAmount+" "+transferFrom+" "+transferTo);
 };
 
 getContract = (web3, address) => {
