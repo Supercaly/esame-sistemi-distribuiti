51d97787e54ca4d990f7b917d3d5d8913ed754a9
Lorenzo Calisti <djonline96@gmail.com>
2021-06-23 10:48:59 +0200

    Change deploy script

--- deploy.js
+++ deploy.js
@@ -1,31 +1,36 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
-const byteCode = fs.readFileSync('./contracts/artifacts/StepCoin.bin');
-const abi = JSON.parse(fs.readFileSync('./contracts/artifacts/StepCoin.abi'));
 
 (async function () {
-  const accounts = await web3.eth.getAccounts();
-  const uploaderAddress = accounts[0];
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const uploaderAddress = accounts[0];
 
-  const contract = new web3.eth.Contract(abi);
+    const abi = JSON.parse(await fs.readFile('./contracts/artifacts/StepCoin.abi'));
+    const contract = new web3.eth.Contract(abi);
+    const byteCode = await fs.readFile('./contracts/artifacts/StepCoin.bin');
 
-  return contract.deploy({
-    data: byteCode.toString(),
-    arguments: [
-      1000,
-      "StepCoin",
-      0,
-      "STP"
-    ]
-  }).send({
-    from: uploaderAddress,
-    gas: 5000000
-  }).then((deployment) => {
+    deployment = await contract.deploy({
+      data: byteCode.toString(),
+      arguments: [
+        1000,
+        "StepCoin",
+        0,
+        "STP"
+      ]
+    }).send({ from: uploaderAddress, gas: 5000000 });
+
     console.log('Contract was successfully deployed!');
-    console.log('Contract can be interfaced with at this address:');
-    console.log(deployment.options.address);
-  }).catch((err) => {
+    console.log('Contract can be interfaced with at this address: '+deployment.options.address);
+
+    await updateDappConfig(deployment.options.address, abi);
+    console.log('Dapp config file successfully generated!');
+  } catch (err) {
     console.error(err);
-  });
+  }
 })();
+
+updateDappConfig = async (contractAddress, abi) => {
+  return fs.writeFile("./dapp/config.js", `config = {"contractAddress": "${contractAddress}", "abiStr": '${JSON.stringify(abi)}'}`);
+};
